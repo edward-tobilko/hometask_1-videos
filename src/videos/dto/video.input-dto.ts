e0c77159b4bd29045e60782c935e72ea406e8344
eduard.tobilko <1992eduard777@gmail.com>
2025-09-18 17:58:22 -0400

    home-task-1

--- src/videos/dto/video.input-dto.ts
+++ src/videos/dto/video.input-dto.ts
@@ -1,1 +1,14 @@
-// ? dto (Data Transfer Object) - то, что присылает клиент
+// ? dto (Data Transfer Object) - то, что присылает клиент (request)
+
+import { VideoInputDto } from '../types/video-input-dto.types';
+import { VideoAvailableResolution } from '../types/video.types';
+
+export const videoInputDto: VideoInputDto = {
+  title: 'Another video-1',
+  author: 'Another author-1',
+  canBeDownloaded: false,
+  minAgeRestriction: null,
+  createdAt: new Date().toISOString(),
+  publicationDate: new Date().toISOString(),
+  availableResolutions: [VideoAvailableResolution.P144],
+};
