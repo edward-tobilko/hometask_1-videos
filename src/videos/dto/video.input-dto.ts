6aa25cd3a564715c72adb9771049be097d13dd54
eduard.tobilko <1992eduard777@gmail.com>
2025-09-20 13:57:30 -0400

    home-task-1-request_objects

--- src/videos/dto/video.input-dto.ts
+++ src/videos/dto/video.input-dto.ts
@@ -1,10 +1,10 @@
 // ? dto (Data Transfer Object) - то, что присылает клиент в body (request)
 
-import { VideoInputDto } from '../types/video-input-dto.types';
+import { CreateVideoInputDto } from '../types/video-input-dto.types';
 import { VideoAvailableResolution } from '../types/video.types';
 
 // * Те, що надсилає клієнт у body (Request body -> Example Value у Swagger):
-export const videoInputDto: VideoInputDto = {
+export const videoInputDto: CreateVideoInputDto = {
   title: 'Another video-1',
   author: 'Another author-1',
   availableResolutions: [VideoAvailableResolution.P144],
