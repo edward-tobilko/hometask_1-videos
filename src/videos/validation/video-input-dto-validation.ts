8c061b7340ec3851454c0f5c6aece1f26e3bd736
eduard.tobilko <1992eduard777@gmail.com>
2025-09-20 13:56:23 -0400

    home-task-1-validations

--- src/videos/validation/video-input-dto-validation.ts
+++ src/videos/validation/video-input-dto-validation.ts
@@ -1,9 +1,12 @@
 import { ValidationError } from '../types/video-errorResponse.types';
-import { VideoInputDto } from '../types/video-input-dto.types';
+import {
+  CreateVideoInputDto,
+  UpdateVideoInputDto,
+} from '../types/video-input-dto.types';
 import { VideoAvailableResolution } from '../types/video.types';
 
-export const videoInputDtoValidation = (
-  data: VideoInputDto,
+export const createVideoInputDtoValidation = (
+  data: CreateVideoInputDto,
 ): ValidationError[] => {
   const errors: ValidationError[] = [];
 
@@ -39,6 +42,76 @@ export const videoInputDtoValidation = (
       field: 'availableResolutions',
       message: 'Invalid resolutions list',
     });
+  }
+
+  return errors;
+};
+
+export const updateVideoInputDtoValidation = (
+  data: UpdateVideoInputDto,
+): ValidationError[] => {
+  const errors: ValidationError[] = [];
+
+  if (
+    !data.title ||
+    typeof data.title !== 'string' ||
+    data.title.trim().length > 40
+  ) {
+    errors.push({
+      field: 'title',
+      message: 'Title length should be no more 40 symbols',
+    });
+  }
+
+  if (
+    !data.author ||
+    typeof data.author !== 'string' ||
+    data.author.trim().length > 20
+  ) {
+    errors.push({
+      field: 'author',
+      message: 'Author must be no more 20 symbols',
+    });
+  }
+
+  if (
+    !Array.isArray(data.availableResolutions) ||
+    !data.availableResolutions.every((currentValue) =>
+      Object.values(VideoAvailableResolution).includes(currentValue),
+    )
+  ) {
+    errors.push({
+      field: 'availableResolutions',
+      message: 'Invalid resolutions list',
+    });
+  }
+
+  if (typeof data.canBeDownloaded !== 'boolean') {
+    errors.push({ field: 'boolean', message: 'must be boolean' });
+  }
+
+  if (
+    typeof data.publicationDate !== 'string' ||
+    Number.isNaN(Date.parse(data.publicationDate))
+  ) {
+    errors.push({
+      field: 'publicationDate',
+      message: 'must be a valid ISO date-time string',
+    });
+  }
+
+  if (data.minAgeRestriction !== null) {
+    if (
+      typeof data.minAgeRestriction !== 'number' ||
+      !Number.isInteger(data.minAgeRestriction) ||
+      data.minAgeRestriction < 1 ||
+      data.minAgeRestriction > 18
+    ) {
+      errors.push({
+        field: 'minAgeRestriction',
+        message: 'must be integer from 1 to 18 or null',
+      });
+    }
   }
 
   return errors;
