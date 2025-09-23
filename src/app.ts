4d76a2001639e57f88ca937c6ebf2ec24a1804f6
eduard.tobilko <1992eduard777@gmail.com>
2025-09-23 11:07:15 -0400

    home-task-1-added root path

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 
 import { testingRouter } from './videos/routers/testing.router';
 import { videosRouter } from './videos/routers/videos.router';
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
+
+  app.get('/', (_req: Request, res: Response) => {
+    res.status(200).send('hello world!!!');
+  });
 
   app.use('/hometask_01/api/testing', testingRouter);
   app.use('/hometask_01/api/videos', videosRouter);
